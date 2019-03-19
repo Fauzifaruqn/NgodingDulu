8a88905e54ad59189d7a4771468c83b3defcc4cd
Fauzifaruqn <fauzifaruqn97@gmail.com>
2019-03-19 14:41:43 +0700

    Smallest Multiple

--- 5_SmallestMultiple.js
+++ 5_SmallestMultiple.js
@@ -5,11 +5,11 @@ function greatestCommon(x, y){
     if (y === 0) return x;
     return greatestCommon(y, x%y);
 }
-function smallestMult(n){
+function smallestMultiple(n){
     let maxLCM = 1;
     for (let i = 2; i <= n; i++){
       maxLCM = lestCommonMultiple(maxLCM, i);
     }
     return console.log(maxLCM);
 }
-smallestMult(5);
+smallestMultiple(5);
